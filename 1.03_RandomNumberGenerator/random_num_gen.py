b7aadce97087bd26696f2bbd6b3cba938695c56b
kpmerry <147255030+kpmerry@users.noreply.github.com>
2025-01-16 04:06:35 +0000

    feat: add random number generator

---
+++ 1.03_RandomNumberGenerator/random_num_gen.py
@@ -0,0 +1,59 @@
+import random
+import time
+
+
+def simple_random():
+    print(
+        "Simple Random Number Generator will now generate a random number based on a given range!"
+    )
+    low = int(input("Choose the lowest number: "))
+    high = int(input("Choose the highest number: "))
+    return print(f"Your number: {random.randint(low, high)}")
+
+
+def random_choice():
+    print("Random choice will pick a random element from a list you enter!")
+    choices = input("Add the list of elements separated by a space: ")
+    choices.split(" ")
+    return print(f"I chose: {random.choice(choices)}")
+
+
+def countdown_letters():
+    """Simulates the game countdown."""
+    print("Let's play countdown! Choose nine letters :)")
+    VOWELS = "a e i o u".upper().split(" ")
+    CONSONANTS = "b c d f g h j k l m n p q r s t v w x y z".upper().split(" ")
+    ALPHABET = "a b c d e f g h i j k l m n o p q r s t u v w x y z".upper().split(" ")
+    letters = []
+    for _ in range(9):
+        choice = input("Vowel (V) or consonant (C)?: ")
+        if choice.upper() == "V":
+            letter = random.choice(VOWELS)
+        elif choice.upper() == "C":
+            letter = random.choice(CONSONANTS)
+        else:
+            print(
+                "Oops, we didn't recognise your command so we just added a random letter :/"
+            )
+            letter = random.choice(ALPHABET)
+        letters.append(letter)
+    print(f"Your letters are: {letters}")
+    for i in range(30, 0, -1):
+        print(i)
+        time.sleep(1)
+    print("Times up!!")
+    return
+
+
+def countdown_numbers():
+    """Simulates the maths round on a game of countdown."""
+    pass
+
+
+def main():
+    print("Welcome to the Random Number Generator!")
+    return countdown_letters()
+
+
+if __name__ == "__main__":
+    main()
