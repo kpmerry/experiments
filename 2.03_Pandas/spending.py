e9f8dbe6fb67b7e6073dd3e3b2674b4105a31c3d
kpmerry <147255030+kpmerry@users.noreply.github.com>
2025-01-19 22:24:47 +0000

    feat: add dataframe for multiple months
    Add a function for checking spending over multiple months and a function for checking spending of a single month.

--- 2.03_Pandas/spending.py
+++ 2.03_Pandas/spending.py
@@ -1,40 +1,86 @@
 import pandas as pd
 import numpy as np
 
-spending = {}
 
-categories = ["Groceries", "Eating Out", "Rent", "Bills", "Health"]
-for category in categories:
-    spending[category] = 0
+# Functions for showing Series of spending:
+def monthly_spending(month_expenses):
+    global m_spending
+    m_spending = {}
+    for c, a in month_expenses:
+        spend(c, a)
+    return show_spending(m_spending)
 
 
 def spend(category: str, amount: int):
-    if category in spending:
-        spending[category] += amount
+    """Adds an expense to a category for a series."""
+    global m_spending
+    if category in m_spending:
+        m_spending[category] += amount
     else:
-        spending[category] = amount
+        m_spending[category] = amount
 
 
-def print_spending(spending):
+def show_spending(spending):
     count = 0
     for category in spending:
         count += spending[category]
     spending["Sum"] = count
-    return print(pd.Series(spending))
+    return pd.Series(spending)
 
 
-jan_spend = [
-    ("Groceries", 45),
-    ("Rent", 450),
-    ("Eating out", 63.43),
-    ("Bills", 20),
-    ("Groceries", 12.43),
-    ("Health", 18.16),
-    ("Maintenance", 3.49),
-    ("Groceries", 2.31),
-]
+# Functions for showing a Data Frame of spending by month.
+def spend_monthly(expenses: list[tuple]):
+    spending = {}
+    for c, a, m in expenses:
+        if not c in spending:
+            spending[c] = {m: a}
+        elif not m in spending[c]:
+            spending[c][m] = a
+        else:
+            spending[c][m] += a
+    return spending
 
-for c, a in jan_spend:
-    spend(c, a)
 
-print_spending(spending)
+def add_month(expenses: list[tuple], month: str):
+    month_expenses = []
+    for c, a in expenses:
+        month_expenses.append((c, a, month))
+    return month_expenses
+
+
+def main():
+    jan_spend = [
+        ("Groceries", 45.21),
+        ("Rent", 450.01),
+        ("Eating out", 63.43),
+        ("Bills", 20.13),
+        ("Groceries", 12.43),
+        ("Health", 18.16),
+        ("Maintenance", 3.49),
+        ("Groceries", 2.31),
+    ]
+
+    feb_spend = [
+        ("Groceries", 35.45),
+        ("Rent", 450),
+        ("Eating out", 23.99),
+        ("Bills", 20),
+        ("Groceries", 12.43),
+        ("Health", 19.22),
+        ("Going out", 7.89),
+        ("Groceries", 5.86),
+        ("Groceries", 12.32),
+    ]
+
+    jan = add_month(jan_spend, "Jan")
+    feb = add_month(feb_spend, "Feb")
+    quarterly_spending = spend_monthly(jan + feb)
+
+    print("\nSpending in January:")
+    print(monthly_spending(jan_spend))
+    print("\nSpending Overall:")
+    print(pd.DataFrame(quarterly_spending, index=["Jan", "Feb"]))
+
+
+if __name__ == "__main__":
+    main()
