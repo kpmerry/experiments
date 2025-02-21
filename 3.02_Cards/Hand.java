d0a62a0adb0b08dca238d8d91dc7890421339aa7
kpmerry <147255030+kpmerry@users.noreply.github.com>
2025-02-21 02:52:18 +0000

    feat: add hand class

--- 3.02_Cards/Hand.java
+++ 3.02_Cards/Hand.java
@@ -1,3 +1,25 @@
-public class Hand {
+import java.util.ArrayList;
 
+public class Hand extends CardContainer {
+    protected String playerName;
+
+    public Hand() {
+        cards = new ArrayList<Card>();
+    }
+
+    public Hand(String inName) {
+        playerName = inName;
+    }
+
+    public String toString() {
+        return "< Hand : " + cards.size() + " >";
+    }
+
+    protected void setName(String inName) {
+        playerName = inName;
+    }
+
+    protected String getName() {
+        return playerName;
+    }
 }
