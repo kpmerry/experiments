f3506b94e155962a64aeebe93dcebf7445b02f6e
kpmerry <147255030+kpmerry@users.noreply.github.com>
2025-03-14 00:37:04 +0000

    feat: add a condensed "fancy" print method

--- 3.02_Cards/Hand.java
+++ 3.02_Cards/Hand.java
@@ -31,13 +31,14 @@ public class Hand extends CardContainer {
     }
 
     public void fancyPrint() {
-        // Print cards with a nice visual.
+        // Print cards with a nice visual in a row.
         StringBuffer s = new StringBuffer();
+        int len = cards.size();
         try {
             s.append("\t" + playerName + "\n");
         } catch (Exception e) {
         }
-        s.append("++===========++  ".repeat(cards.size()));
+        s.append("++===========++  ".repeat(len));
         s.append("\n");
         for (Card c : cards) {
             if (c.hidden == true) {
@@ -61,14 +62,57 @@ public class Hand extends CardContainer {
                 s.append("||  ");
             }
         }
+        for (int i = 0; i < 3; i++) {
+            s.append("\n");
+            s.append("||           ||  ".repeat(len));
+        }
         s.append("\n");
-        s.append("||           ||  ".repeat(cards.size()));
+        s.append("++===========++  ".repeat(len));
         s.append("\n");
-        s.append("||           ||  ".repeat(cards.size()));
+        System.out.print(s);
+    }
+
+    public void condensedFancyPrint() {
+        int len = cards.size();
+        // Print cards in a row with condensed format.
+        StringBuffer s = new StringBuffer();
+        try {
+            s.append("\t" + playerName + "\n");
+        } catch (Exception e) {
+        }
+        s.append("++==========".repeat(len));
+        s.append("++");
         s.append("\n");
-        s.append("||           ||  ".repeat(cards.size()));
+        for (Card c : cards) {
+            if (c.hidden == true) {
+                s.append("||   hidden ");
+            } else {
+                s.append("||     ");
+                s.append(c.getRank());
+                s.append(" ".repeat(5 - c.getRank().length()));
+            }
+        }
+        s.append("||");
         s.append("\n");
-        s.append("++===========++  ".repeat(cards.size()));
+        for (Card c : cards) {
+            if (c.hidden == true) {
+                s.append("||   hidden ");
+            } else {
+                String suit = c.getSuit();
+                s.append("||  ");
+                s.append(suit);
+                s.append(" ".repeat(8 - suit.length()));
+            }
+        }
+        s.append("||");
+        for (int i = 0; i < 3; i++) {
+            s.append("\n");
+            s.append("||          ".repeat(len));
+            s.append("||");
+        }
+        s.append("\n");
+        s.append("++==========".repeat(len));
+        s.append("++");
         s.append("\n");
         System.out.print(s);
     }
